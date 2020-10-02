c35b7c029b3c3f0f45fbf380591af19452d43e55
Giuseppe Diciolla <giuseppe.diciolla@gmail.com>
2020-10-02 11:44:21 +0200

    [master] mobile phone call action

--- FindYourFriends/FindYourFriends/View/DetailCard.swift
+++ FindYourFriends/FindYourFriends/View/DetailCard.swift
@@ -7,12 +7,15 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class DetailCard: UIViewController{
     
     @IBOutlet weak var nomeutente: UILabel!
     @IBOutlet weak var callCTA: UIButton!
     @IBOutlet weak var navigateCTA: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var userStreet: UILabel!
     
     var detailData: Friend?
     
@@ -20,6 +23,29 @@ class DetailCard: UIViewController{
         super.viewDidLoad()
         
         nomeutente.text = detailData?.name
+        userStreet.text = detailData?.street
+        callCTA.layer.cornerRadius = 25
+        navigateCTA.layer.cornerRadius = 25
         
+        callCTA.addTarget(self, action: #selector(callAction), for: .touchUpInside)
+        navigateCTA.addTarget(self, action: #selector(mapAction), for: .touchUpInside)
+        
+    }
+    
+     @objc func callAction(sender: UIButton!) {
+        let formattedNumber = detailData?.phone!.components(separatedBy: NSCharacterSet.decimalDigits.inverted).joined(separator: "")
+
+        if let url = NSURL(string: ("tel:" + (formattedNumber)!)) {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url as URL)
+            }
+        }
+        print("Button tapped")
+     }
+    
+    @objc func mapAction(sender: UIButton!) {
+       print("Button tapped")
     }
 }
